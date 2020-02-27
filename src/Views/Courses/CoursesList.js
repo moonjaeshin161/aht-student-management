e21a780b36a676eccd46055f453969332e6cf1ff
TungBicepm <congtunggv@gmail.com>
2020-02-27 17:05:36 +0700

    dung concat

--- src/Views/Courses/CoursesList.js
+++ src/Views/Courses/CoursesList.js
@@ -2,16 +2,12 @@ import React from 'react'
 import CoursesItem from './CoursesItem';
 
 const CoursesList = ({ courses }) => {
-    const _courses = courses.state;
-    console.log({ _courses });
-
-
     return (
         <>
             <div>Courses List</div>
             <div>
                 {
-                    _courses?.map((course, index) => {
+                    courses?.map((course, index) => {
                         return (
                             <CoursesItem course={course} key={index} />
                         )
--- src/Views/Courses/CoursesReducer.js
+++ src/Views/Courses/CoursesReducer.js
@@ -1,7 +1,7 @@
 export const CoursesReducer = (state, action) => {
     switch (action.type) {
         case 'GET_COURSES':
-            return { ...state, state: action.payload };
+            return state.concat(action.payload);
         default:
             throw new Error();
     }
