90cd8597fb3f6301cc88db743034bb29c7dd4b75
moonjaeshin161 <huyvd.uet.vnu@gmail.com>
2020-02-27 12:21:52 +0700

    testing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+{ AD }
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
